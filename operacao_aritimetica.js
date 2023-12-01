b9509fd02ad0b5c28a24bf7ffdf70bda6a98964a
maria <maria_f_dias@estudante.sesisenai.org.br>
2023-12-01 11:19:15 -0300

    segunda versão - declaração de funções

--- operacao_aritimetica.js
+++ operacao_aritimetica.js
@@ -1,7 +1,13 @@
 let res = document.getElementById('res')
 
 function soma(){
-    
+   let num1 = Number(document.getElementById('num1').value) 
+   let num2 = Number(document.getElementById('num2').value) 
+
+   let resultadoSoma = num1 + num2
+
+   res.innerHTMLL = 'A soma de ' + num1 +'com' + num2 + 'é =' + resultadoSoma.toFixed(2)
+
 }
 function subtração(){
 
